b63d5baa64383dfe12485b5b5220d335ce5302e1
Michael Merry Okungbowa <michaelmerry8@gmail.com>
2022-12-11 17:24:25 +0000

    Applied Data Science Assignment 2 Second Commit

--- DataScience.py
+++ DataScience.py
@@ -3,3 +3,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns 
 import matplotlib.patches as mpatches
+
+def file_reader(path):
+    """
+    This goal of this function is to read csv file and returns two dataframe. 
+    Args: 
+        path (str) : This is csv file path
+        
+    Return : 
+        This function return an original dataframe(df) and a transposed dataframe(df_t).
+    """
+    df = pd.read_csv(path, skiprows = 4, index_col = [])
+    df.drop(['Indicator Code', 'Country Code', 'Unnamed: 66'], axis = 1, inplace = True)
+    df_i = df.set_index(['Country Name', 'Indicator Name'])
+    df_t = df_i.T
+    return df, df_t
